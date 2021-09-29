1bf8632b7ca24efb26a0f72c1bb3ef3d0838859d
Nicanor ZINKO <parmenaszinko@gmail.com>
2021-09-29 06:24:45 +0000

    Add destroy admin function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
-class User < ApplicationRecord
-    
+class User < ApplicationRecord 
+    before_update :destroy_admin
+    before_create :destroy_admin
     validates :name, presence:true, length: {minimum:3, maximum:30}
     validates :email, presence:true, length: { maximum:255 }, 
                 format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
@@ -8,6 +9,11 @@ class User < ApplicationRecord
     has_secure_password
     has_many :tasks, dependent: :destroy
 
-   
+    private
+    def destroy_admin
+        if self.admin 
+            User.update_all(admin: 0)
+        end
+    end
     
 end
