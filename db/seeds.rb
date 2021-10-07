9f087a0492dc7eb8a4eaf7a84759c391aca5fada
Nicanor ZINKO <parmenaszinko@gmail.com>
2021-10-07 01:42:54 +0000

    Add db:seed to create user and tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
-User.create!(name: "Nicanor", email: "parmenaszinko@gmail.com", password: "nica1002")
+10.times do |n|
+    name = Faker::Games::Pokemon.name
+    email = Faker::Internet.email
+    password = "mypassword"
+    User.create!(name: name,
+                email: email,
+                password: password,)
+end
 
+10.times do |n|
+    name = Faker::Games::Pokemon.name
+    Tag.create!(name: name)
+end
