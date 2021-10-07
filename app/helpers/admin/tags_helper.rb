c3bd6af56901d3c82e301bef55d00c89362102b6
Nicanor ZINKO <parmenaszinko@gmail.com>
2021-10-07 01:58:57 +0000

    Fixed Error

--- app/helpers/admin/tags_helper.rb
+++ app/helpers/admin/tags_helper.rb
@@ -1,2 +1,9 @@
 module Admin::TagsHelper
+    def choose_new_or_edit
+        if action_name == 'new'
+            admin_tags_path
+        elsif action_name == 'edit'
+            admin_tag_path    
+        end
+    end
 end
